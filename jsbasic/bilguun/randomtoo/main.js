eb644ba1208e9f61769fadb77b39fd346cb39b0e
Yalalt <yalaltgandush@gmail.com>
2022-12-01 23:01:29 +0800

    Random numbers generate

---
+++ jsbasic/bilguun/randomtoo/main.js
@@ -0,0 +1,8 @@
+console.log("1 - 100 хүртэл санамсаргүй тоо");
+let ramToo = Math.floor((Math.random() * 100) + 1);
+console.log(": " + ramToo);
+
+console.log("\n50 - 255 хүртэл санамсаргүй тоо");
+ramToo = Math.floor(Math.random() * (255 - 50 + 1) + 50);
+console.log(": " + ramToo);
+
