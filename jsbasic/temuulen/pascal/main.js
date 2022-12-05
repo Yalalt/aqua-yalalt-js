1fec6d53b40d1d9be2247d85daab45539e034205
Yalalt <yalaltgandush@gmail.com>
2022-12-05 22:44:21 +0800

    pascal toonii triangle uusgej hiiw

--- jsbasic/temuulen/pascal/main.js
+++ jsbasic/temuulen/pascal/main.js
@@ -1,27 +1,21 @@
-let preNum = 1;
-let currNum = 1;
-let afterNum = 0;
-let rowNum = 7;
+let pNum = 1;
+let rowNum = 6;
 let pascalNumbers = "";
 
-for (var i = 1; i <= rowNum; i++) {
+debugger;
+for (var i = 0; i < rowNum; i++) {
   for (var j = rowNum - i; j > 0; j--) {
     pascalNumbers += " ";
   }
-  for (let number = 0; number < i; number++) {
+  pNum = 1;
+  for (let number = 0; number <= i; number++) {
     if (number == 0 || number == i - 1) {
-      pascalNumbers += `${preNum} `;
-      
+      pascalNumbers += `${pNum} `;
     } else {
-        afterNum = preNum + currNum;
-        if(number == afterNum) {
-            pascalNumbers += `${i - 1} `;
-        } else {
-            pascalNumbers += `${i} `;
-        }
+      pascalNumbers += `${pNum} `;
     }
+    pNum = pNum * (i - number)/(number + 1);
   }
   pascalNumbers += "\n";
 }
-
 console.log(pascalNumbers);
