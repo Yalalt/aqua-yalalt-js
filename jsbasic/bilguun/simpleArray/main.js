fa661ca8fb8feb3667094cae57a2ddbbad019f6c
Yalalt <yalaltgandush@gmail.com>
2022-12-05 23:45:05 +0800

    It Company names add remove pop push splice hiiw

--- jsbasic/bilguun/simpleArray/main.js
+++ jsbasic/bilguun/simpleArray/main.js
@@ -1,4 +1,4 @@
-let companies = [
+let itCompanies = [
   "Facebook",
   "Google",
   "Twitter",
@@ -11,16 +11,38 @@ let companies = [
 
 let cutedBegin3 = [3];
 for (let i = 0; i < 3; i++) {
-  cutedBegin3[i] = companies[i];
+  cutedBegin3[i] = itCompanies[i];
 }
+console.log(itCompanies);
 console.log("Эхний 3 компани: " + cutedBegin3);
 
 let suul3company = [3];
 debugger;
 for (let icom = 1; icom < 4; icom++) {
-  suul3company[icom - 1] = companies[companies.length - icom];
+  suul3company[icom - 1] = itCompanies[itCompanies.length - icom];
 }
 
 console.log("\nСүүлийн 3 компани: " + suul3company);
+console.log("\nКомпани дундаас нь авч хаях");
 
-console.log("Компани дундаас авч хаях");
+let remCompanyNames = ["Apple", "Google"];
+console.log("Авч хаях компани " + remCompanyNames + "\n");
+
+for (let rem = 0; rem < remCompanyNames.length; rem++) {
+  let index = itCompanies.indexOf(remCompanyNames[rem]);
+
+  if(index !== -1) {
+    itCompanies.splice(index, 1);
+  }
+}
+console.log(itCompanies);
+
+console.log("\nЭхний компанийг хасах: ");
+itCompanies.shift();
+
+console.log(itCompanies);
+
+console.log("Сүүлийн компанийг хасах: ");
+itCompanies.pop();
+
+console.log(itCompanies);
