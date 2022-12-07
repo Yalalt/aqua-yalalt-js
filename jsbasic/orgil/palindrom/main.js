e4a34ae6a283666ba0ddfeea0d60ab0669521f37
Yalalt <yalaltgandush@gmail.com>
2022-12-07 13:22:42 +0800

    Palindrome check function create

--- jsbasic/orgil/palindrom/main.js
+++ jsbasic/orgil/palindrom/main.js
@@ -1,20 +1,15 @@
-const palindromeCheck = function (n, a, b) {
-  let result;
-  let firstIndex = 0;
-  let lastIndex = n.toString().length - 1;
-  if (n !== 0) {
-  }
-  if (n[firstIndex] == n[lastIndex]) {
-    palindromeCheck();
-  }
+const palindromeCheck = function () {
+
 };
 
-let test1 = "abcddcba";
+
+
+let test1 = "bbcddcba";
 let test2 = 1234567654321;
-let a = test1.length;
-let b = test1.length;
-let testFunction1 = palindromeCheck(test1, a, b);
-// let testFunction2 = palindromeCheck(test2);
+let a = 0;
+let b = test1.length - 1;
+let count = Math.floor(b - a)/2;
+let testFunction1 = palindromeCheck(test1, a, b, count);
 
 console.log(testFunction1);
-// console.log(testFunction2);
+
