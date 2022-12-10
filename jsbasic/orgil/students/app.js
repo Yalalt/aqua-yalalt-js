ddcc80c41f34c57ce96623fb44925b795d3a2efa
Yalalt <yalaltgandush@gmail.com>
2022-12-10 20:01:06 +0800

    Students data file created Teacher Orgil

---
+++ jsbasic/orgil/students/app.js
@@ -0,0 +1,4 @@
+const studentsData = [
+    {name: 'Gonchigsumlai', email: 'gonchigsumlai@gmail.com', birthYear: 1998, hobby: "Basketball", single: false},
+    {name: 'Gonchigsumlai', email: 'gonchigsumlai@gmail.com', birthYear: 1998, hobby: "Basketball", single: false},
+];
