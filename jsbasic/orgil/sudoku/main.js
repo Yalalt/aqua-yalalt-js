0e42f29d04da103f499ab4ff988737191cd46d2b
Yalalt <yalaltgandush@gmail.com>
2022-12-20 18:44:10 +0800

    Sudoku game begin

--- jsbasic/orgil/sudoku/main.js
+++ jsbasic/orgil/sudoku/main.js
@@ -1,25 +1,69 @@
-let numbers = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
 let board = [
-  [numbers, numbers, numbers],
-  [numbers, numbers, numbers],
-  [numbers, numbers, numbers],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
-let index = Math.floor(Math.random() * 9 + 1);
-let rooot = document.getElementById('root');
 
-function generateField(board){
+let root = document.getElementById("root");
+let selectedElement;
 
-    for(let i = 0; i < board.length; i++){
-        for(let j = 0; j < board[i].length; j++){
-            for(let k =  0; k < board[i][j].length; k++){
-                let newBox = document.createElement('div');
-                newBox.classList.add('field');
-                rooot.appendChild(newBox);
-            }
-        }
+function generateFields(board) {
+  let sudokuField = document.createElement("div");
+  sudokuField.setAttribute("id", "sudokuField");
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      let newDiv = document.createElement("div");
+      newDiv.setAttribute("class", "field");
+      newDiv.setAttribute("id", i + "-" + j);
+      newDiv.addEventListener("click", () => {
+        console.log(newDiv.id);
+        selectedElement = [parseInt(newDiv.id[0]), parseInt(newDiv.id[2])];
+      });
+      if (board[i][j] !== 0) {
+        newDiv.innerText = board[i][j];
+      }
+      sudokuField.appendChild(newDiv);
     }
+  }
+  root.appendChild(sudokuField);
 }
+
+function generateButtons() {
+  let buttons = document.createElement("div");
+  buttons.setAttribute("id", "buttons");
+
+  for (let i = 0; i < 9; i++) {
+    let newDiv = document.createElement("button");
+    newDiv.setAttribute("class", "button");
+    newDiv.innerText = i + 1;
+    buttons.appendChild(newDiv);
+  }
+  root.appendChild(buttons);
+}
+
+function initialGame() {
+  //random numbe generate fill board[0][0] -> board[2][2]
+  //davhtsahgui 1-9 hoornd random too generate hiine
+}
+
+function validation(board) {
+  // end shalgana true false butsaana
+  // jishee n shalgah 3n nohtsol baigaa
+  // center 5 iin too oruulhad
+  // (center squere- d 5n too banyy?) || (i==4) uyd 5n too bainyy || (j==4) uyd 5n too bainyy
+}
+
+generateFields(board);
+generateButtons();
+
+document.getElementById("test").addEventListener("click", () => {
+  board[selectedElement[0]][selectedElement[1]] = "8";
+  generateFields(board);
+});
