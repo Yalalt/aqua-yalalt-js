ce9b4222435cbbca8a89687794721ae73da43322
Yalalt <yalaltgandush@gmail.com>
2022-12-23 00:41:12 +0800

    sudoku edited

--- jsbasic/orgil/games/sudoku/main.js
+++ jsbasic/orgil/games/sudoku/main.js
@@ -53,11 +53,56 @@ function initialGame() {
   //davhtsahgui 1-9 hoornd random too generate hiine
 }
 
-function validation(board) {
+
   // end shalgana true false butsaana
   // jishee n shalgah 3n nohtsol baigaa
   // center 5 iin too oruulhad
   // (center squere- d 5n too banyy?) || (i==4) uyd 5n too bainyy || (j==4) uyd 5n too bainyy
+function isValidSudoku(grid) {
+  // Check rows
+  for (let i = 0; i < 9; i++) {
+    let row = new Set();
+    for (let j = 0; j < 9; j++) {
+      if (grid[i][j] !== 0) {
+        if (row.has(grid[i][j])) {
+          return false;
+        }
+        row.add(grid[i][j]);
+      }
+    }
+  }
+
+  // Check columns
+  for (let i = 0; i < 9; i++) {
+    let column = new Set();
+    for (let j = 0; j < 9; j++) {
+      if (grid[j][i] !== 0) {
+        if (column.has(grid[j][i])) {
+          return false;
+        }
+        column.add(grid[j][i]);
+      }
+    }
+  }
+
+  // Check 3x3 sub-grids
+  for (let i = 0; i < 9; i += 3) {
+    for (let j = 0; j < 9; j += 3) {
+      let subGrid = new Set();
+      for (let k = 0; k < 3; k++) {
+        for (let l = 0; l < 3; l++) {
+          if (grid[i + k][j + l] !== 0) {
+            if (subGrid.has(grid[i + k][j + l])) {
+              return false;
+            }
+            subGrid.add(grid[i + k][j + l]);
+          }
+        }
+      }
+    }
+  }
+
+  return true;
 }
 
 generateFields(board);
@@ -67,3 +112,9 @@ document.getElementById("test").addEventListener("click", () => {
   board[selectedElement[0]][selectedElement[1]] = "8";
   generateFields(board);
 });
+
+if(isValidSudoku(board)){
+  console.log("Zow baina");
+} else {
+  console.log("Buruu too oruulsan bna");
+}
