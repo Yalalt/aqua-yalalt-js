fdd3d8c92232b8216de0a99690428a512fb4c3a3
Yalalt <yalaltgandush@gmail.com>
2022-12-26 10:22:57 +0800

    lesson filescreated

---
+++ jsbasic/orgil/ec7/main.js
@@ -0,0 +1,17 @@
+let person = {
+    name: "john",
+    age: 29,
+    gender: 'male',
+    grade1: 2.9,
+    grade2: 2.9,
+    grade3: 2.9,
+    grade4: 2.9,
+    grade5: 2.9,
+    grade6: 2.9,
+}
+
+console.log(person);
+
+let {grade1, grade2, grade3, grade4, grade5, grade6} = person;
+
+console.log(grade1, grade2, grade3, grade4, grade5, grade6);
